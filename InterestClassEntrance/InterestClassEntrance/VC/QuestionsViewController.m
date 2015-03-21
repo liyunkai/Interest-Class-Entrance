7a91dfa6c9e29c080a770799c7fe3ec696706b44
李允恺(Yunkai Li) <jfunsir@gmail.com>
2015-03-21 16:59:49 +0800

    基本解决滑动切换的动画BUG

--- InterestClassEntrance/InterestClassEntrance/VC/QuestionsViewController.m
+++ InterestClassEntrance/InterestClassEntrance/VC/QuestionsViewController.m
@@ -25,13 +25,26 @@
     [self.view addGestureRecognizer:panGesture];
     
     self.seq = 1;
-    CGRect visitedFrame = self.view.frame;
+    CGRect visitedFrame = [UIScreen mainScreen].bounds;
     self.visitedView = [[QuestionView alloc] initWithFrame:visitedFrame];
     [self.visitedView awakeFromNib];
     self.nextView = [[QuestionView alloc] initWithFrame:CGRectMake(visitedFrame.origin.x + visitedFrame.size.width, visitedFrame.origin.y, visitedFrame.size.width, visitedFrame.size.height)];
     [self.nextView awakeFromNib];
-    [self.view addSubview:self.nextView];
     [self.view addSubview:self.visitedView];
+    [self.view addSubview:self.nextView];
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    [self fuzhi];
+}
+
+-(void)fuzhi{
+    self.visitedView.labelA.text = [NSString stringWithFormat:@"%d",self.seq];
+    if (self.seq!=1) {
+        self.preView.labelA.text = [NSString stringWithFormat:@"%d",self.seq-1];
+    }
+    if (self.seq!=100) {
+        self.nextView.labelA.text = [NSString stringWithFormat:@"%d",self.seq+1];
+    }
 }
 
 - (void)handlePan:(UIPanGestureRecognizer *)recognizer{
@@ -41,86 +54,137 @@
     {
         CGFloat screenWidth_2 = [UIScreen mainScreen].bounds.size.width/2.0f;
         if (translation.x < -screenWidth_2) {//手势平移超过一半，进入下一题
-            [self nextQuestion];
+            if (self.seq == 100) {
+                [[[UIAlertView alloc] initWithTitle:nil message:@"这是最后一题" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil, nil] show];
+                [self resetView];
+                return;
+            }
+            [self nextQuestionViewState];
         }
         else if( translation.x > screenWidth_2 )
         {//回到上一题
-            [self nextQuestion];
+            if (self.seq == 1) {
+                [[[UIAlertView alloc] initWithTitle:nil message:@"这是第一题" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil, nil] show];
+                [self resetView];
+                return;
+            }
+            [self preQuestionViewState];
         }
         else
         {//复位
             [self resetView];
-            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 20, 200, 20)];
-            label.text = @"hello world";
-            [self.view addSubview:label];
         }
+        [self fuzhi];
     }
 }
 
-- (void) nextQuestion{
-    self.seq = self.seq++;
-    [UIView beginAnimations:@"nextQuestion" context:nil];
-    [UIView setAnimationDuration:1];
-    [self moveViewWithTranslation:CGPointMake(self.visitedView.frame.origin.x-self.view.bounds.size.width, 0)];
-    [UIView commitAnimations];
-    if (self.seq!=1) {
-        [self.preView removeFromSuperview];
+- (void) moveViewWithTranslation:(CGPoint)translation{
+    if(self.seq != 1 ) {
+           self.preView.transform = CGAffineTransformMakeTranslation(translation.x, 0);
     }
-    self.preView = self.visitedView;
-    self.visitedView = self.nextView;
-    self.nextView = [[QuestionView alloc] initWithFrame:CGRectMake(self.view.frame.origin.x + self.view.frame.size.width, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
-    [self.nextView awakeFromNib];
-    [self.view addSubview:self.nextView];
-}
-
-- (void) preQuestion{
-    self.seq = self.seq--;
-    [UIView beginAnimations:@"preQuestion" context:nil];
-    [UIView setAnimationDuration:1];
-    [self moveViewWithTranslation:CGPointMake(self.view.frame.size.width-self.visitedView.frame.origin.x, 0)];
-    [UIView commitAnimations];
     if (self.seq != 100) {
-        [self.nextView removeFromSuperview];
+        self.nextView.transform = CGAffineTransformMakeTranslation(translation.x, 0);
     }
-    self.nextView = self.visitedView;
-    self.visitedView = self.preView;
-    self.preView = [[QuestionView alloc] initWithFrame:CGRectMake(self.view.frame.origin.x - self.view.frame.size.width, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
-    [self.preView awakeFromNib];
+    self.visitedView.transform = CGAffineTransformMakeTranslation(translation.x, 0);
+}
+
+- (void) nextQuestionViewState{
+    [UIView animateWithDuration:0.5f animations:^{
+        self.visitedView.transform = CGAffineTransformMakeTranslation(-[UIScreen mainScreen].bounds.size.width, 0);
+        self.nextView.transform = CGAffineTransformMakeTranslation(-[UIScreen mainScreen].bounds.size.width, 0);
+        if (self.seq == 1) {
+            self.preView.transform = CGAffineTransformMakeTranslation(-[UIScreen mainScreen].bounds.size.width, 0);
+        }
+    } completion:^(BOOL finished) {
+        if (finished) {
+            QuestionView *dequeueQuestionView;
+            if (self.seq==1) {
+                dequeueQuestionView = [[QuestionView alloc] init];
+                [dequeueQuestionView awakeFromNib];
+                [self.view addSubview:dequeueQuestionView];
+            }else{
+                dequeueQuestionView = self.preView;
+            }
+            self.preView = self.visitedView;
+            self.visitedView = self.nextView;
+            self.nextView = nil;
+            self.nextView = dequeueQuestionView;
+            [self adjustViewAfterStep];
+            
+            self.seq = self.seq+1;
+            if (self.seq == 100) {
+                [self.nextView removeFromSuperview];
+                self.nextView = nil;
+            }
+        }
+    }];
+}
+
+- (void) preQuestionViewState{
+    [UIView animateWithDuration:0.5f animations:^{
+        self.visitedView.transform = CGAffineTransformMakeTranslation([UIScreen mainScreen].bounds.size.width, 0);
+        self.preView.transform = CGAffineTransformMakeTranslation([UIScreen mainScreen].bounds.size.width, 0);
+        if (self.seq == 100) {
+            self.nextView.transform = CGAffineTransformMakeTranslation([UIScreen mainScreen].bounds.size.width, 0);
+        }
+    } completion:^(BOOL finished) {
+        if (finished) {
+            QuestionView *dequeueQuestionView;
+            if (self.seq==100) {
+                dequeueQuestionView = [[QuestionView alloc] init];
+                [dequeueQuestionView awakeFromNib];
+                [self.view addSubview:dequeueQuestionView];
+            }else{
+                dequeueQuestionView = self.nextView;
+            }
+            self.nextView = self.visitedView;
+            self.visitedView = self.preView;
+            self.preView = nil;
+            self.preView = dequeueQuestionView;
+            [self adjustViewAfterStep];
+            
+            self.seq = self.seq-1;
+            if (self.seq == 1) {
+                [self.preView removeFromSuperview];
+                self.preView = nil;
+            }
+        }
+    }];
+}
+
+- (void)adjustViewAfterStep{
+    [self.preView removeFromSuperview];
+    self.preView.transform = CGAffineTransformIdentity;
+    self.preView.frame = CGRectMake([UIScreen mainScreen].bounds.origin.x - [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.origin.y, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
     [self.view addSubview:self.preView];
+    
+    [self.visitedView removeFromSuperview];
+    self.visitedView.transform = CGAffineTransformIdentity;
+    self.visitedView.frame = [UIScreen mainScreen].bounds;
+    [self.view addSubview:self.visitedView];
+    
+    [self.nextView removeFromSuperview];
+    self.nextView.transform = CGAffineTransformIdentity;
+    self.nextView.frame = CGRectMake([UIScreen mainScreen].bounds.origin.x+[UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.origin.y, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
+    [self.view addSubview:self.nextView];
+
+    
 }
 
 - (void) resetView{
     [UIView beginAnimations:@"resetView" context:nil];
     [UIView setAnimationDuration:1];
     if (self.seq != 1) {
-        self.preView.center = CGPointMake(self.view.center.x - self.view.frame.size.width, self.view.center.y);
+        self.preView.transform = CGAffineTransformIdentity;
     }
     if (self.seq != 100) {
-        self.nextView.center = CGPointMake(self.view.center.x + self.view.frame.size.width, self.view.center.y);
+        self.nextView.transform = CGAffineTransformIdentity;
     }
-    self.visitedView.center = self.view.center;
+    self.visitedView.transform = CGAffineTransformIdentity;
     [UIView commitAnimations];
 }
 
-- (void) moveViewWithTranslation:(CGPoint)translation{
-    if (translation.x>0) {
-        if(self.seq == 1 ) {
-            [[[UIAlertView alloc] initWithTitle:nil message:@"已经是第一题" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil, nil] show];
-            return;
-        }else{
-            self.preView.center =CGPointMake(self.visitedView.center.x+translation.x, self.visitedView.center.y);
-        }
-    }
-    if (translation.x <0) {
-        if (self.seq == 100) {
-            [[[UIAlertView alloc] initWithTitle:nil message:@"已经是最后一题" delegate:self cancelButtonTitle:@"知道了" otherButtonTitles:nil, nil] show];
-            return;
-        }else{
-            self.nextView.center = CGPointMake(self.visitedView.center.x+translation.x, self.visitedView.center.y);
-        }
-    }
-    self.visitedView.center = CGPointMake(self.visitedView.center.x+translation.x, self.visitedView.center.y);
-}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
