87b5395cf1dd391f00a4730844fcca9f38e74701
Shaojun <yushaojun@huakexunce.com>
2015-03-26 23:26:07 +0800

    no message

--- InterestClassEntrance/InterestClassEntrance/VC/ReportViewController.m
+++ InterestClassEntrance/InterestClassEntrance/VC/ReportViewController.m
@@ -14,6 +14,10 @@
 @property (weak, nonatomic) IBOutlet ASProgressPopUpView *progressView1;
 @property (weak, nonatomic) IBOutlet ASProgressPopUpView *progressView2;
 @property (weak, nonatomic) IBOutlet ASProgressPopUpView *progressView3;
+@property (strong, nonatomic) IBOutlet ASProgressPopUpView *progressView4;
+@property (strong, nonatomic) IBOutlet ASProgressPopUpView *progressView5;
+@property (strong, nonatomic) IBOutlet ASProgressPopUpView *progressView6;
+@property (strong, nonatomic) IBOutlet ASProgressPopUpView *progressView7;
 
 
 
@@ -26,28 +30,55 @@
     // Do any additional setup after loading the view.
     UIImage *bgColor = [UIImage imageNamed:@"beijing.png"];
     self.view.layer.contents = (id)bgColor.CGImage;
-    
+    [self progressViewInit];
+    NSLog(@"%d",self.gress[0]);
+}
+
+-(void)progressViewInit{
     self.progressView1.popUpViewCornerRadius = 12;
-    self.progressView1.popUpViewAnimatedColors = @[[UIColor redColor], [UIColor orangeColor], [UIColor greenColor]];
+//    UIColor *color1 = [UIColor colorWithPatternImage:[UIImage imageNamed:@"1.png"]];
+    self.progressView1.popUpViewAnimatedColors = @[[UIColor brownColor]];
     self.progressView1.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
-    [self.progressView1 setProgress:0.9 animated:YES];
+    [self.progressView1 setProgress:self.gress[0]/100.0 animated:YES];
     [self.progressView1 showPopUpViewAnimated:YES];
     
     self.progressView2.popUpViewCornerRadius = 12.0;
-    self.progressView2.popUpViewAnimatedColors = @[[UIColor redColor], [UIColor orangeColor], [UIColor greenColor]];
+    self.progressView2.popUpViewAnimatedColors = @[[UIColor blueColor]];
     self.progressView2.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
-    [self.progressView2 setProgress:0.8 animated:YES];
+    [self.progressView2 setProgress:self.gress[1]/100.0 animated:YES];
     [self.progressView2 showPopUpViewAnimated:YES];
-    
+//
     self.progressView3.popUpViewCornerRadius = 12.0;
-    self.progressView3.popUpViewAnimatedColors = @[[UIColor redColor], [UIColor orangeColor], [UIColor greenColor]];
+    self.progressView3.popUpViewAnimatedColors = @[[UIColor brownColor]];
     self.progressView3.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
-    [self.progressView3 setProgress:0.6 animated:YES];
+    [self.progressView3 setProgress:self.gress[2]/100.0 animated:YES];
     [self.progressView3 showPopUpViewAnimated:YES];
-
+//
+    self.progressView4.popUpViewCornerRadius = 12.0;
+    self.progressView4.popUpViewAnimatedColors = @[[UIColor grayColor]];
+    self.progressView4.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
+    [self.progressView4 setProgress:self.gress[3]/100.0 animated:YES];
+    [self.progressView4 showPopUpViewAnimated:YES];
+    
+    self.progressView5.popUpViewCornerRadius = 12.0;
+    self.progressView5.popUpViewAnimatedColors = @[[UIColor orangeColor]];
+    self.progressView5.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
+    [self.progressView5 setProgress:self.gress[4]/100.0 animated:YES];
+    [self.progressView5 showPopUpViewAnimated:YES];
+    
+    self.progressView6.popUpViewCornerRadius = 12.0;
+    self.progressView6.popUpViewAnimatedColors = @[[UIColor yellowColor]];
+    self.progressView6.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
+    [self.progressView6 setProgress:self.gress[5]/100.0 animated:YES];
+    [self.progressView6 showPopUpViewAnimated:YES];
+    
+    self.progressView7.popUpViewCornerRadius = 12.0;
+    self.progressView7.popUpViewAnimatedColors = @[[UIColor greenColor]];
+    self.progressView7.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:15];
+    [self.progressView7 setProgress:self.gress[6]/100.0 animated:YES];
+    [self.progressView7 showPopUpViewAnimated:YES];
+    
 }
-
-
 // <ASProgressPopUpViewDataSource> is entirely optional
 // it allows you to supply custom NSStrings to ASProgressPopUpView
 - (NSString *)progressView:(ASProgressPopUpView *)progressView stringForProgress:(float)progress
